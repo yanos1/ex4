09b7a818f4537a5af4cabf542230fce8bc66d57a
GoldenSpringFox <46161299+GoldenSpringFox@users.noreply.github.com>
2024-03-16 16:46:29 +0200

    avatar animations

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -3,16 +3,25 @@ package pepse.world;
 import danogl.GameObject;
 import danogl.gui.ImageReader;
 import danogl.gui.UserInputListener;
+import danogl.gui.rendering.AnimationRenderable;
 import danogl.util.Vector2;
 
 import java.awt.event.KeyEvent;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.function.Consumer;
 
 /**
  * Class of the simulations main playable character.
  */
 public class Avatar extends GameObject {
-    public static final String IDLE_SPRITE_FILE = "assets/idle_0.png";
+    public static final String[] IDLE_SPRITE_FILES =
+            {"assets/idle_0.png", "assets/idle_1.png", "assets/idle_2.png", "assets/idle_3.png"};
+    public static final String[] RUN_SPRITE_FILES =
+            {"assets/run_0.png", "assets/run_1.png", "assets/run_2.png", "assets/run_3.png",
+                    "assets/run_4.png", "assets/run_5.png"};
+    public static final String[] JUMP_SPRITE_FILES =
+            {"assets/jump_0.png", "assets/jump_1.png", "assets/jump_2.png", "assets/jump_3.png"};
     public static final int SPRITE_WIDTH = 50;
     public static final int SPRITE_HEIGHT = 50;
     private static final float VELOCITY_X = 400;
@@ -23,6 +32,7 @@ public class Avatar extends GameObject {
     private static final float JUMP_ENERGY_LOSS = 10;
     private static final float MAX_ENERGY = 100;
     private static final float MIN_ENERGY = 0;
+    private static final float TIME_BETWEEN_ANIMATIONS = 0.1f;
 
     private enum MovementState { IDLE, RUN, JUMP };
 
@@ -30,7 +40,12 @@ public class Avatar extends GameObject {
     private float energy;
     private MovementState state;
 
+    private final AnimationRenderable idleAnimation;
+    private final AnimationRenderable runAnimation;
+    private final AnimationRenderable jumpAnimation;
+
     private Consumer<Integer> energyChangeCallback;
+    private final List<Runnable> jumpCallbacks = new LinkedList<>();
 
     /**
      * Constructor.
@@ -41,11 +56,20 @@ public class Avatar extends GameObject {
     public Avatar (Vector2 pos, UserInputListener inputListener, ImageReader imageReader) {
         super(pos.subtract(new Vector2(SPRITE_WIDTH, SPRITE_HEIGHT)),
                 new Vector2(SPRITE_WIDTH, SPRITE_HEIGHT),
-                imageReader.readImage(IDLE_SPRITE_FILE, false));
+                imageReader.readImage(IDLE_SPRITE_FILES[0], false));
         physics().preventIntersectionsFromDirection(Vector2.ZERO);
         transform().setAccelerationY(GRAVITY);
+
         this.inputListener = inputListener;
         energy = MAX_ENERGY;
+        state = MovementState.IDLE;
+
+        idleAnimation = new AnimationRenderable(
+                IDLE_SPRITE_FILES, imageReader, false, TIME_BETWEEN_ANIMATIONS);
+        runAnimation = new AnimationRenderable(
+                RUN_SPRITE_FILES, imageReader, false, TIME_BETWEEN_ANIMATIONS);
+        jumpAnimation = new AnimationRenderable(
+                JUMP_SPRITE_FILES, imageReader, false, TIME_BETWEEN_ANIMATIONS);
     }
 
     public void SetEnergyChangeCallback(Consumer<Integer> energyChangeCallback) {
@@ -54,6 +78,24 @@ public class Avatar extends GameObject {
 
     public float GetEnergy() {
         return energy;
+    }
+
+    public void addEnergy(float amount) {
+        energy += amount;
+        if (energy < MIN_ENERGY) {
+            energy = MIN_ENERGY;
+        }
+        else if (energy > MAX_ENERGY) {
+            energy = MAX_ENERGY;
+        }
+
+        if (energyChangeCallback != null) {
+            energyChangeCallback.accept((int)energy);
+        }
+    }
+
+    public void addJumpCallback(Runnable callback) {
+        jumpCallbacks.add(callback);
     }
 
     /**
@@ -73,6 +115,7 @@ public class Avatar extends GameObject {
         updateRunning();
         updateJumping();
         updateEnergy();
+        updateAnimation();
     }
 
     private void updateRunning() {
@@ -105,23 +148,36 @@ public class Avatar extends GameObject {
         {
             state = MovementState.JUMP;
             transform().setVelocityY(VELOCITY_Y);
+            for (Runnable callback : jumpCallbacks) {
+                callback.run();
+            }
         }
     }
 
     private void updateEnergy() {
         switch(state) {
             case MovementState.IDLE:
-                energy = Math.min(MAX_ENERGY, energy + IDLE_ENERGY_GAIN);
+                addEnergy(IDLE_ENERGY_GAIN);
                 break;
             case MovementState.RUN:
-                energy = Math.max(MIN_ENERGY, energy - RUN_ENERGY_LOSS);
+                addEnergy(-RUN_ENERGY_LOSS);
                 break;
             case MovementState.JUMP:
-                energy = Math.max(MIN_ENERGY, energy - JUMP_ENERGY_LOSS);
+                addEnergy(-JUMP_ENERGY_LOSS);
                 break;
         }
-        if (energyChangeCallback != null) {
-            energyChangeCallback.accept((int)energy);
+    }
+
+    private void updateAnimation() {
+        if (transform().getVelocity().x() != 0) {
+            renderer().setRenderable(runAnimation);
+            renderer().setIsFlippedHorizontally(transform().getVelocity().x() < 0);
+        }
+        else if (transform().getVelocity().y() != 0) {
+            renderer().setRenderable(jumpAnimation);
+        }
+        else {
+            renderer().setRenderable(idleAnimation);
         }
     }
 }
